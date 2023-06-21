2329ff095c16de9e7f67b2f84fdb1bbe5bccd120
WhyJam <137121221+WhyJam@users.noreply.github.com>
2023-06-21 23:00:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -44,3 +44,12 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 };
+document.addEventListener("DOMContentLoaded", function() {
+            var menuToggle = document.getElementById("menu-toggle");
+            var nav = document.querySelector("nav");
+
+            menuToggle.addEventListener("click", function() {
+                nav.classList.toggle("active");
+                menuToggle.classList.toggle("active");
+            });
+        });
