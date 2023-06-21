62d821652bf06017ea71cf92318400e13799529a
WhyJam <137121221+WhyJam@users.noreply.github.com>
2023-06-21 23:38:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const menuToggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
+<script>
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
 
-menuToggle.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
+    menuToggle.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    });
+</script>
