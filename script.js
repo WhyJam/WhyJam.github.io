02f2b9f551bd83e92c72ba4446b0b279a14933ba
WhyJam <137121221+WhyJam@users.noreply.github.com>
2023-06-24 15:55:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,26 @@
 const menuToggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu');
-const navLinks = document.querySelectorAll('#menu a');
+const menuItems = menu.getElementsByTagName('a');
+const navBarHeight = menu.offsetHeight;
+const offsetValue = 30; // Adjust this value as needed
 
 menuToggle.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
 
-navLinks.forEach(link => {
-    link.addEventListener('click', (event) => {
+// Smooth scroll to the section with offset
+for (let i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener('click', (event) => {
         event.preventDefault();
-        event.stopPropagation();
+        const targetId = menuItems[i].getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        const targetPosition = targetSection.offsetTop - navBarHeight - 30;
+
+        window.scrollTo({
+            top: targetPosition,
+            behavior: 'smooth'
+        });
+
         menu.classList.remove('active');
     });
-});
-
-document.body.addEventListener('click', (event) => {
-    if (!menu.contains(event.target) && menu.classList.contains('active')) {
-        menu.classList.remove('active');
-    }
-});
-
-const menuButton = document.querySelector('.menu-toggle');
-const header = document.querySelector('header');
-
-menuButton.addEventListener('click', function () {
-    menuButton.classList.toggle('active');
-    header.classList.toggle('visible');
-});
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    var menuToggle = document.querySelector(".menu-toggle");
-    var menu = document.querySelector("#menu");
-
-    menuToggle.addEventListener("click", function() {
-        menu.classList.toggle("active");
-    });
-});
+}
