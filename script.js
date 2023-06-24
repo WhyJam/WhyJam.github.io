cf1d32ac749b28dbf80d1fd71b7c222db9b6d490
WhyJam <137121221+WhyJam@users.noreply.github.com>
2023-06-24 15:01:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-<script>
-    const menuToggle = document.getElementById('menu-toggle');
-    const menu = document.getElementById('menu');
+const menuToggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('menu');
+const navLinks = document.querySelectorAll('#menu a');
 
-    menuToggle.addEventListener('click', () => {
-        menu.classList.toggle('active');
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        menu.classList.remove('active');
     });
+});
 
-    const navLinks = document.querySelectorAll('#menu a');
+document.body.addEventListener('click', (event) => {
+    if (!menu.contains(event.target) && menu.classList.contains('active')) {
+        menu.classList.remove('active');
+    }
+});
 
-    navLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            menu.classList.remove('active');
-        });
+const menuButton = document.querySelector('.menu-toggle');
+const header = document.querySelector('header');
+
+menuButton.addEventListener('click', function () {
+    menuButton.classList.toggle('active');
+    header.classList.toggle('visible');
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var menuToggle = document.querySelector(".menu-toggle");
+    var menu = document.querySelector("#menu");
+
+    menuToggle.addEventListener("click", function() {
+        menu.classList.toggle("active");
     });
-</script>
+});
